257df0cf67741c0a3a36305c778a10eda595a064
AlinaMaican <alinutzamaican@yahoo.com>
2019-04-08 23:15:39 +0300

    reports

--- SSS/config/routes.rb
+++ SSS/config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
+  get 'reports/index'
   get 'dashboard/index'
   get 'users/index'
 
-<<<<<<< HEAD
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }
 
-=======
-  devise_for :users, controllers: { :omniauth_callbacks => "omniauth" }
- 
->>>>>>> a7408b1a4d1f50881360e5253910444c3ba6fc1f
   root to: 'home#index'
 
   root to: 'users#index'
