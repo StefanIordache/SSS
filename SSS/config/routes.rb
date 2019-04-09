725f0b4641edc833ae53badf9ac08f16250ad082
StefanIordache <iordachestefan1@gmail.com>
2019-04-09 14:30:07 +0300

    Home Page design completed and tested

--- SSS/config/routes.rb
+++ SSS/config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
 
-  root to: 'users#index'
+  # root to: 'users#index'
 
-  root to: 'dashboard#index'
+  # root to: 'dashboard#index'
 
 end
